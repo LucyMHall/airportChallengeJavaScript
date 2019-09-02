c047e70076324cf9065609ff4dedcf9a18170c8e
LucyMHall <lucymhall1995@gmail.com>
2019-09-02 09:43:40 +0100

    Task: rewind code so that #land is the main function being tested

--- __tests__/airport.test.js
+++ __tests__/airport.test.js
@@ -5,20 +5,12 @@ describe('#getNumberOfPlanes', () => {
     const airportA = new Airport();
     expect(airportA.getNumberOfPlanes()).toBe(0);
   });
-  it('increments by 1 for every plane landed', () => {
+});
+
+describe('#land', () => {
+  it('increases the number of planes in an airport by 1', () => {
     const airportA = new Airport();
-    const fakePlane = {};
-    airportA.land(fakePlane);
+    airportA.land();
     expect(airportA.getNumberOfPlanes()).toBe(1);
-  });
-  it('does not increase beyond the capacity', () => {
-    const airportA = new Airport();
-    const fakePlane1 = {};
-    airportA.land(fakePlane1);
-    const fakePlane2 = {};
-    airportA.land(fakePlane2);
-    const fakePlane3 = {};
-    airportA.land(fakePlane3);
-    expect(airportA.getNumberOfPlanes()).toBe(2);
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,18 +1,14 @@
 class Airport {
-  constructor(capacity = 2) {
+  constructor() {
     this.numberOfPlanes = 0;
-    this.capacity = capacity;
   }
 
   getNumberOfPlanes() {
     return this.numberOfPlanes;
   }
 
-  land(plane) {
-    if (this.numberOfPlanes !== this.capacity) {
-      this.numberOfPlanes += 1;
-    }
-    return this.numberOfPlanes;
+  land() {
+    this.numberOfPlanes += 1;
   }
 }
 exports.Airport = Airport;
